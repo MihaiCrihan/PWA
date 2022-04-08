51e8d6fe19aac8711b9f1037a79b444af9265f52
Mihai <haos-sah@mail.ru>
2022-04-08 21:28:38 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from "./components/MainPage";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    MainPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
+
+<template>
+  <v-app >
+    <v-main>
+      <MainPage />
+    </v-main>
+  </v-app>
+</template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #efefef;
 }
 </style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,58 +1,165 @@
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+
+  data: () => ({
+    users: ["1M", "2D", "3D", "4F", "5D", "6D", "7F"],
+    menuButtons: ["mdi-home", "mdi-magnify", "mdi-plus", "mdi-heart", "mdi-cog"],
+  }),
+};
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="users-story d-flex">
+      <div class="add-story">
+        <v-avatar color="primary" size="54" class="mr-4 add-story__inner"> CM</v-avatar>
+        <v-btn
+            fab
+            x-small
+            width="18"
+            height="18"
+            text
+            dark
+            smal
+            class="primary add-story__button"
+            color="white"
+        >
+          <v-icon x-small>
+            mdi-plus
+          </v-icon>
+        </v-btn>
+      </div>
+      <v-avatar
+        color="primary"
+        size="54"
+        class="mr-4"
+        v-for="(item) in 10"
+        :key="item"
+      >
+        <img src="https://picsum.photos/200" alt="">
+      </v-avatar>
+    </div>
+
+    <div class="post-container pb-10">
+      <v-card elevation="0" class="mb-4" v-for="item in 5" :key="item">
+        <v-card-title>
+          <v-avatar color="primary" size="36" class="mr-4">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+          <div>Crihan Mihai</div>
+        </v-card-title>
+        <v-card-text>
+          <iframe
+            width="100%"
+            height="400px"
+            src="https://www.youtube.com/embed/w9okGAKOyYk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn fab text dark small color="pink">
+            <v-icon dark> mdi-heart </v-icon>
+          </v-btn>
+          <v-btn fab text dark small color="pink">
+            <v-icon dark> mdi-share </v-icon>
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn fab text dark small color="black">
+            <v-icon dark> mdi-plus </v-icon>
+          </v-btn>
+        </v-card-actions>
+        <div class="px-4 pb-5">1032 Отметок нравится</div>
+      </v-card>
+    </div>
+
+    <div class="bottom-menu d-flex justify-space-around align-center">
+      <v-btn
+        fab
+        text
+        dark
+        small
+        color="black"
+        v-for="(item, index) in menuButtons"
+        :key="index"
+      >
+        <v-icon dark>
+          {{ item }}
+        </v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<style scoped lang="scss">
+* {
+  ::-webkit-scrollbar {
+    width: 0;
+    height: 0;
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+.add-story {
+  position: relative;
+
+  .v-avatar:after {
+    display: none;
+  }
+
+  &__inner {}
+  &__button {
+    position: absolute;
+    right: 12px;
+    bottom: 0;
+
+    &::v-deep .v-btn__content {
+      border: 1px solid lightgray;
+      border-radius: 100%;
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.users-story {
+  padding: 16px;
+  overflow-x: scroll;
+  border-bottom: 1px solid lightgray;
+
+  .user {
+    margin-right: 12px;
+  }
+
+  .v-avatar {
+    position: relative;
+    overflow: visible;
+
+    &:after {
+      position: absolute;
+      top: -4px;
+      left: -4px;
+      width: 62px;
+      height: 62px;
+      content: " ";
+      border: 2px solid pink;
+      border-radius: 100%;
+    }
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.bottom-menu {
+  position: fixed;
+  height: 48px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+  border-top: 1px solid lightgray;
+  background: white;
+  width: 100%;
 }
 </style>
